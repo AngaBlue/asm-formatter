06b069cbadccbd434d93a667d18b8c455189832c
AngaBlue <contact@anga.blue>
2023-10-28 20:00:14 +1100

    Desired output

--- test/labels.asm
+++ test/labels.asm
@@ -1,8 +1,8 @@
 .data   
 LabelVeryLong:  .word   5
 LabelShort:     .word   10
-AnotherLabel:   .asciiz "This is a string"
-LabelMed:    .word   20
+AnotherLabel:   .asciiz "This is a string#;.,\"f"
+LabelMed:       .word   20
 SomeLabel:      .word   1, 2, 3, 4          # Comment after data
 
 .text   
@@ -10,8 +10,8 @@ main:
     li      $t0,    5                       # Load immediate
     add     $t1,    $t0,    $zero           ; Another way to comment
 
-# Blank line above
-SomeLabel:
+# Blank line above, at the end of a label block
+SomeLabel:                                  # Comment here too
     sub     $t2,    $t1,    $t0             ; Subtract operation
 
 AnotherFunction:
@@ -19,7 +19,7 @@ AnotherFunction:
     mflo    $t3
     syscall 
 
-# Another comment line here
+    # Another comment line here, but this time its inside a label
     sw      $t3,    0($sp)
 
 # End of the file.
